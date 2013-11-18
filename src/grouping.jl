ad3a0f7293fbd91ff58ebd89476fe0769a11247c
John Myles White <jmw@johnmyleswhite.com>
2013-11-17 20:50:53 -0800

    Clean up comment style in grouping.jl

--- src/grouping.jl
+++ src/grouping.jl
@@ -1,21 +1,21 @@
-function groupsort_indexer(x::AbstractVector, ngroups::Int)
-    ## translated from Wes McKinney's groupsort_indexer in pandas (file: src/groupby.pyx).
+function groupsort_indexer(x::AbstractVector, ngroups::Integer)
+    # translated from Wes McKinney's groupsort_indexer in pandas (file: src/groupby.pyx).
 
-    ## count group sizes, location 0 for NA
+    # count group sizes, location 0 for NA
     n = length(x)
-    ## counts = x.pool
+    # counts = x.pool
     counts = fill(0, ngroups + 1)
     for i = 1:n
         counts[x[i] + 1] += 1
     end
 
-    ## mark the start of each contiguous group of like-indexed data
+    # mark the start of each contiguous group of like-indexed data
     where = fill(1, ngroups + 1)
     for i = 2:ngroups+1
         where[i] = where[i - 1] + counts[i - 1]
     end
     
-    ## this is our indexer
+    # this is our indexer
     result = fill(0, n)
     for i = 1:n
         label = x[i] + 1
@@ -24,4 +24,5 @@ function groupsort_indexer(x::AbstractVector, ngroups::Int)
     end
     result, where, counts
 end
+
 groupsort_indexer(pv::PooledDataVector) = groupsort_indexer(pv.refs, length(pv.pool))
