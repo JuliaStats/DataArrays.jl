8d322be59b1167c1ab550fb268a7878b042cfbad
Colin Beckingham <colbec@kingston.net>
2016-12-30 11:41:38 -0500

    Update predicates.jl

--- src/predicates.jl
+++ src/predicates.jl
@@ -1,6 +1,10 @@
-using Primes
-numeric_predicates = (:(Base.iseven), :(Base.ispow2), :(Base.isfinite), :(Primes.isprime), :(Base.isinf), :(Base.isodd))
-
+if VERSION < v"0.6.0-dev+1781"
+  numeric_predicates = (:(Base.iseven), :(Base.ispow2), :(Base.isfinite), :(Base.isprime), :(Base.isinf), :(Base.isodd))
+else
+  using Primes
+  numeric_predicates = (:(Base.iseven), :(Base.ispow2), :(Base.isfinite), :(Primes.isprime), :(Base.isinf), :(Base.isodd))
+endif
+  
 type_predicates = (:(Base.isinteger), :(Base.isreal))
 
 container_predicates = (:(Base.isempty),)
