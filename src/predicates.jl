f46d1e0f84b4450deb5de01e46bc4185a9ab7418
Colin Beckingham <colbec@kingston.net>
2017-01-01 03:13:03 -0500

    Update predicates.jl
    Using code contributed by @ararslan

--- src/predicates.jl
+++ src/predicates.jl
@@ -1,9 +1,5 @@
-if VERSION < v"0.6.0-dev+1781"
-  numeric_predicates = (:(Base.iseven), :(Base.ispow2), :(Base.isfinite), :(Base.isprime), :(Base.isinf), :(Base.isodd))
-else
-  import Primes
-  numeric_predicates = (:(Base.iseven), :(Base.ispow2), :(Base.isfinite), :(Primes.isprime), :(Base.isinf), :(Base.isodd))
-end
+numeric_predicates = [:(Base.iseven), :(Base.ispow2), :(Base.isfinite), :(Base.isinf), :(Base.isodd))]
+isdefined(Base, :isprime) && push!(numeric_predicates, :(Base.isprime))
   
 type_predicates = (:(Base.isinteger), :(Base.isreal))
 
